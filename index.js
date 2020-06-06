c5f66dc3c4e62cbd9f620a31e65ced1b1b2d2395
zaephyr <uros.mahne@gmail.com>
2020-06-06 16:37:35 +0200

    only optional left

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 let container = document.getElementById('container');
+let kids = container.childNodes;
 const root = document.documentElement;
-const promptSide = parseInt(prompt('How many per side?: '));
 
-root.style.setProperty('--side', promptSide);
-const grid = promptSide * promptSide;
+function clicked() {
+    const promptSide = parseInt(prompt('How many per side?: '));
 
-for (let i = 0; i < grid; i++) {
-    const div = document.createElement('div');
-    div.id = i;
-    div.style.width = '100%';
-    div.style.height = '100%';
-    container.append(div);
+    root.style.setProperty('--side', promptSide);
+    const grid = promptSide * promptSide;
+
+    for (let i = 0; i < grid; i++) {
+        const div = document.createElement('div');
+        div.style.width = '100%';
+        div.style.height = '100%';
+        container.append(div);
+
+        if (kids[i].nodeName.toLowerCase() == 'div') {
+            kids[i].style.background = '#f3f3f3';
+        }
+    }
 }
-
 container.addEventListener('mouseover', function (event) {
     event.target.style.backgroundColor = 'cadetblue';
 });
